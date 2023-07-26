b8d1b7ad111a63421d98d1c5d0cd745b70282426
avena <fernando.avena@gmail.com>
2023-07-25 23:49:22 -0300

    Lista de dados

---
+++ avancando/listas.php
@@ -0,0 +1,7 @@
+<?php
+
+// $idadeList =  array(21, 23, 19, 25, 30, 41, 18); // metodo mais antigo para vetor/array
+$idadeList = [21, 23, 19, 25, 30, 41, 18];
+$primeiraIdade = $idadeList[0];
+
+echo $primeiraIdade;
